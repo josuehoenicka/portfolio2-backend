d5e4edb66ec3419f30febd6eb979dd74645c2ad1
Josué Hoenicka <josuee.vzla@gmail.com>
2022-11-14 01:34:37 -0300

    create: first commit

---
+++ src/test/java/com/josuehoenicka/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josuehoenicka.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
